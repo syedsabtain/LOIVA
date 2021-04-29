2896bd3a046c63064c0a3879f85f053bd9bb0079
Syed <sabtain.info@gmail.com>
2021-04-30 00:13:54 +0500

    replaced img

--- src/AtomComponent/OutTeamCard.js
+++ src/AtomComponent/OutTeamCard.js
@@ -6,7 +6,7 @@ const OurTeamCard = ({Mainclass,titleClass,paragraphClass, emailClass, iconClass
         <div  className={`m-auto md:ml-4 md:mr-4 w-full mt-5 md:mt-0 text-white   h-full p-3 lg:w-2/5 sm:w-4/4 md:w-2/4 rounded text-center py-8 bg-secondary ${Mainclass}`}
                   style={customeStyle}>
 
-                  <div className="flex justify-center mb-2"> <img  alt={title} style={{backgroundImage:`url(${imgSrc})`}} className="rounded-full bg-center bg-no-repeat  bg-cover h-24 w-24 flex items-center justify-center" /> </div>
+                  <div className="flex justify-center mb-2"> <div  alt={title} style={{backgroundImage:`url(${imgSrc})`}} className="rounded-full bg-center bg-no-repeat  bg-cover h-24 w-24 flex items-center justify-center" /> </div>
                   <h1 className={`text-3xl ${titleClass}`}>{title}</h1> 
                   <div className={`text-sm pt-3 md:w-80 m-auto ${paragraphClass} overflow-y-hidden`} style={{height:'80px'}}>{paragraph}</div>
                   <div className={`pt-5 hover:underline cursor-pointer  font-semibold ${emailClass}`}>{email}</div>
